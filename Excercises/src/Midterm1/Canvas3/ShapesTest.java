cf970bfa316fd0578505a68f1650906edc2263d0
anastasristeski <anastas.risteski5@gmail.com>
2024-12-13 23:35:35 +0100

    Partially correct

--- Excercises/src/Midterm1/Canvas3/ShapesTest.java
+++ Excercises/src/Midterm1/Canvas3/ShapesTest.java
@@ -1,9 +1,109 @@
 package Midterm1.Canvas3;
-
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
-
 enum Color {
     RED, GREEN, BLUE
+}
+interface Scalable{
+    public void scale(float scaleFactor);
+
+}
+interface Stackable{
+    public float weight();
+}
+abstract class Shape implements Scalable,Stackable{
+    String id;
+    Color color;
+    public Shape(String id,Color color){
+        this.id = id;
+        this.color= color;
+    }
+    @Override
+    abstract public void scale(float scaleFactor);
+
+    @Override
+    abstract public float weight();
+}
+class Circle extends Shape{
+    float radius;
+
+    public Circle(String id, Color color,float radius) {
+        super(id, color);
+        this.radius = radius;
+    }
+
+    @Override
+    public void scale(float scaleFactor) {
+        radius +=scaleFactor;
+    }
+
+    @Override
+    public float weight() {
+        return (float) (Math.PI * radius * radius);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("C: %-5s%10s%10.2f\n",id,color.toString(),weight());
+    }
+}
+class Rectangle extends Shape{
+    float width;
+    float height;
+    public Rectangle(String id, Color color,float width,float height) {
+        super(id, color);
+        this.width = width;
+        this.height = height;
+    }
+    @Override
+    public void scale(float scaleFactor) {
+        width += scaleFactor;
+        height += scaleFactor;
+    }
+
+    @Override
+    public float weight() {
+        return width * height;
+    }
+    @Override
+    public String toString() {
+        return String.format("R: %-5s%10s%10.2f\n",id,color.toString(),weight());
+    }
+}
+class Canvas {
+    ArrayList<Shape> shapes;
+    public Canvas(){
+        shapes = new ArrayList<>();
+    }
+    public void add(String id,Color color,float radius){
+        Circle c = new Circle(id,color,radius);
+        int i=0;
+        while(i<shapes.size() && shapes.get(i).weight()>=c.weight())
+            i++;
+        shapes.add(i,c);
+    }
+    public void add(String id,Color color,float width,float height){
+        Rectangle r = new Rectangle(id,color,width,height);
+        int i=0;
+        while(i<shapes.size() && shapes.get(i).weight()>=r.weight())
+            i++;
+        shapes.add(i,r);
+    }
+    public void scale(String id,float scaleFactor){
+        for (Shape shape : shapes) {
+            if (shape.id.equals(id))
+                shape.scale(scaleFactor);
+        }
+        shapes.sort(Comparator.comparing(Shape::weight).reversed());
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        shapes.forEach(sb::append);
+        return sb.toString();
+    }
 }
 public class ShapesTest {
     public static void main(String[] args) {
@@ -36,10 +136,3 @@ public class ShapesTest {
     }
 }
 
-enum Color {
-    RED, GREEN, BLUE
-}
-
-class Canvas {
-
-}
