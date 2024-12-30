871d9a8f113899a393181a8515f45b82b5267630
anastasristeski <anastas.risteski5@gmail.com>
2024-12-30 02:15:26 +0100

    solved 5th lab

--- Excercises/src/LABS/LAB5/TermFrequency/TermFrequencyTest.java
+++ Excercises/src/LABS/LAB5/TermFrequency/TermFrequencyTest.java
@@ -2,18 +2,40 @@ package LABS.LAB5.TermFrequency;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeMap;
-import java.util.TreeSet;
-import java.util.Map.Entry;
+import java.util.*;
+import java.util.stream.Collectors;
 
+class TermFrequency{
+    Map<String,Integer> wordsMap;
+    public TermFrequency(InputStream inputStream, String[] stopWords) {
+       wordsMap = new TreeMap<>();
+       Scanner sc = new Scanner(inputStream);
+       while(sc.hasNext()){
+           String word = sc.next();
+           word = word.toLowerCase()
+                   .replaceAll("[,.]","")
+                   .trim();
+          if(!(word.isEmpty() || Arrays.asList(stopWords).contains(word))){
+               wordsMap.putIfAbsent(word,0);
+               wordsMap.put(word,wordsMap.get(word)+1);
+           }
+       }
+    }
+
+    public int countTotal(){
+        return wordsMap.values().stream().mapToInt(Integer::intValue).sum();
+    }
+    public int countDistinct(){
+        return wordsMap.size();
+    }
+    public List<String> mostOften(int k){
+        return wordsMap.keySet()
+                .stream()
+                .sorted(Comparator.comparing(x->wordsMap.get(x)).reversed())
+                .limit(k)
+                .collect(Collectors.toList());
+    }
+}
 public class TermFrequencyTest {
     public static void main(String[] args) throws FileNotFoundException {
         String[] stop = new String[] { "во", "и", "се", "за", "ќе", "да", "од",
@@ -23,7 +45,7 @@ public class TermFrequencyTest {
                 stop);
         System.out.println(tf.countTotal());
         System.out.println(tf.countDistinct());
-        System.out.println(tf.mostOften(10));
+         System.out.println(tf.mostOften(10));
     }
 }
 // vasiot kod ovde
